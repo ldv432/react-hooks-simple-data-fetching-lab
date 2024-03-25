561ed35f08960b57e8d5090fed59ce0072402e1b
ldv432 <ldv432@gmail.com>
2024-03-25 14:54:46 -0600

    doggos

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useEffect, useState} from 'react'
+
+export default function App() {
+    const [message, setMessage] = useState(null)
+   
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((r) => r.json())
+          .then(data => setMessage(data.message));
+          }, []);
+
+      if(!message){
+        return <p>Loading...</p>
+      }
+      
+  return (
+    <div>
+      <p>Dog Image</p>
+      <img src={message} alt="A Random Dog"/>
+    </div>
+  )
+}
